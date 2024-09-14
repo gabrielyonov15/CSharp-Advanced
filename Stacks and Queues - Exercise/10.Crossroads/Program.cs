823f120c9dfb384334e41e29f249c9f20df78317
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-09-14 16:46:11 +0300

    Add files via upload

---
+++ Stacks and Queues - Exercise/10.Crossroads/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace _10.Crossroads
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int greenLightDuration = int.Parse(Console.ReadLine());
+            int freeWindowDuration = int.Parse(Console.ReadLine());
+            Queue<string> carsQueue = new Queue<string>();
+            int totalCarsPassed = 0;
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "END")
+                {
+                    Console.WriteLine("Everyone is safe.");
+                    Console.WriteLine($"{totalCarsPassed} total cars passed the crossroads.");
+                    break;
+                }
+                if (input == "green")
+                {
+                    int currentGreenTime = greenLightDuration;
+                    while (carsQueue.Count > 0 && currentGreenTime > 0)
+                    {
+                        string currentCar = carsQueue.Dequeue();
+                        if (currentCar.Length <= currentGreenTime)
+                        {
+                            currentGreenTime -= currentCar.Length;
+                            totalCarsPassed++;
+                        }
+                        else
+                        {
+                            int remainingTime = currentGreenTime;
+                            int remainingLength = currentCar.Length - remainingTime;
+                            if (remainingLength <= freeWindowDuration)
+                            {
+                                totalCarsPassed++;
+                            }
+                            else
+                            {
+                                char hitChar = currentCar[remainingTime + freeWindowDuration];
+                                Console.WriteLine("A crash happened!");
+                                Console.WriteLine($"{currentCar} was hit at {hitChar}.");
+                                return;
+                            }
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    carsQueue.Enqueue(input);
+                }
+            }
+        }
+    }
+}
---
+++ Stacks and Queues - Exercise/11.KeyRevolver/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _11.KeyRevolver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int bulletPrice = int.Parse(Console.ReadLine());  
+            int barrelSize = int.Parse(Console.ReadLine());  
+            Stack<int> bullets = new Stack<int>(Array.ConvertAll(Console.ReadLine().Split(), int.Parse)); 
+            Queue<int> locks = new Queue<int>(Array.ConvertAll(Console.ReadLine().Split(), int.Parse));  
+            int intelligenceValue = int.Parse(Console.ReadLine());
+            int bulletsUsed = 0;  
+            int currentBarrel = barrelSize;  
+            while (locks.Count > 0 && bullets.Count > 0)
+            {
+                int currentBullet = bullets.Pop();  
+                int currentLock = locks.Peek();    
+                bulletsUsed++;  
+                currentBarrel--; 
+                if (currentBullet <= currentLock)
+                {
+                    Console.WriteLine("Bang!");
+                    locks.Dequeue();  
+                }
+                else
+                {
+                    Console.WriteLine("Ping!");
+                }
+                if (currentBarrel == 0 && bullets.Count > 0)
+                {
+                    Console.WriteLine("Reloading!");
+                    currentBarrel = barrelSize;  
+                }
+            }
+            if (locks.Count == 0)
+            {
+                int moneyEarned = intelligenceValue - (bulletsUsed * bulletPrice);
+                Console.WriteLine($"{bullets.Count} bullets left. Earned ${moneyEarned}");
+            }
+            else
+            {
+                Console.WriteLine($"Couldn't get through. Locks left: {locks.Count}");
+            }
+        }
+    }
+}
