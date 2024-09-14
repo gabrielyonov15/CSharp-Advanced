bdfb74746d73b5bee6741f55a1c78cdd7518583a
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-09-14 16:40:24 +0300

    Add files via upload

---
+++ Stacks and Queues - Lab/8.TrafficJam/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _8.TrafficJam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Queue<string> carsQueue = new Queue<string>();
+            int totalCarsPassed = 0;
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "end")
+                {
+                    Console.WriteLine($"{totalCarsPassed} cars passed the crossroads.");
+                    break;
+                }
+                else 
+                if (command == "green")
+                {
+                    for (int i = 0; i < n && carsQueue.Count > 0; i++)
+                    {
+                        string car = carsQueue.Dequeue();
+                        Console.WriteLine($"{car} passed!");
+                        totalCarsPassed++; 
+                    }
+                }
+                else
+                {
+                    carsQueue.Enqueue(command);
+                }
+            }
+        }
+    }
+}
