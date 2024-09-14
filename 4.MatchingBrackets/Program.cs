eb3c7fcd7d35e94fee4bdf4231d2269bbdd7e467
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-09-14 16:37:10 +0300

    Add files via upload

---
+++ 4.MatchingBrackets/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _4.MatchingBrackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<int> stack = new Stack<int>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                {
+                    stack.Push(i);
+                }
+                else
+                if (input[i] == ')')
+                {
+                    int startIndex = stack.Pop();
+                    int endIndex = i;
+                    string subExpression = input.Substring(startIndex, endIndex - startIndex + 1);
+                    Console.WriteLine(subExpression);
+                }
+            }
+        }
+    }
+}
