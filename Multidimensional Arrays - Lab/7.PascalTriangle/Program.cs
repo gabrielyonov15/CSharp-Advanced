d0c51120763c095b72f065b09d19cacdf4c21440
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-09-14 16:49:53 +0300

    Add files via upload

---
+++ Multidimensional Arrays - Lab/7.PascalTriangle/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _7.PascalTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            long[][] pascalTriangle = new long[n][];
+            for (int i = 0; i < n; i++)
+            {
+                pascalTriangle[i] = new long[i + 1];
+                pascalTriangle[i][0] = 1;
+                pascalTriangle[i][i] = 1;
+                for (int j = 1; j < i; j++)
+                {
+                    pascalTriangle[i][j] = pascalTriangle[i - 1][j - 1] + pascalTriangle[i - 1][j];
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(string.Join(" ", pascalTriangle[i]));
+            }
+        }
+    }
+}
