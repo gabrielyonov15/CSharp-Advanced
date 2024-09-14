c8088b8828a9077029c8ea7c8ced4249fc4200b2
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-09-14 16:39:37 +0300

    Add files via upload

---
+++ Stacks and Queues - Lab/7.HotPotato/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _7.HotPotato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> children = new Queue<string>(Console.ReadLine().Split());
+            int n = int.Parse(Console.ReadLine());
+            while (children.Count > 1)
+            {
+                for (int i = 1; i < n; i++)
+                {
+                    string currentChild = children.Dequeue(); 
+                    children.Enqueue(currentChild);          
+                }
+                Console.WriteLine($"Removed {children.Dequeue()}");
+            }
+            Console.WriteLine($"Last is {children.Dequeue()}");
+        }
+    }
+}
