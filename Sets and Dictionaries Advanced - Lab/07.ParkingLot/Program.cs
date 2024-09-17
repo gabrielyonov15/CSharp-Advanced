311eb03d2dc1bf0c209ab6cfaf1e08d13894d182
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-09-17 18:20:26 +0300

    Add files via upload

---
+++ Sets and Dictionaries Advanced - Lab/07.ParkingLot/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _07.ParkingLot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var parkingLot = new HashSet<string>();
+            string input;
+            while ((input = Console.ReadLine()) != "END")
+            {
+                var parts = input.Split(", ");
+                string direction = parts[0];
+                string carNumber = parts[1];
+                if (direction == "IN")
+                {
+                    parkingLot.Add(carNumber);
+                }
+                else 
+                if (direction == "OUT")
+                {
+                    parkingLot.Remove(carNumber);
+                }
+            }
+            if (parkingLot.Count > 0)
+            {
+                foreach (var car in parkingLot)
+                {
+                    Console.WriteLine(car);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+        }
+    }
+}
---
+++ Sets and Dictionaries Advanced - Lab/08.SoftUniParty/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _08.SoftUniParty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var vipGuests = new HashSet<string>();
+            var regularGuests = new HashSet<string>();
+            string input;
+            bool partyStarted = false;
+            while ((input = Console.ReadLine()) != "END")
+            {
+                if (input == "PARTY")
+                {
+                    partyStarted = true;
+                    continue;
+                }
+
+                if (partyStarted)
+                {
+                    vipGuests.Remove(input);
+                    regularGuests.Remove(input);
+                }
+                else
+                {
+                    if (char.IsDigit(input[0]))
+                    {
+                        vipGuests.Add(input);
+                    }
+                    else
+                    {
+                        regularGuests.Add(input);
+                    }
+                }
+            }
+            var notArrivedGuests = vipGuests.Concat(regularGuests).ToList();
+            Console.WriteLine(notArrivedGuests.Count);
+            foreach (var guest in notArrivedGuests)
+            {
+                Console.WriteLine(guest);
+            }
+        }
+    }
+}
