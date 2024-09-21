d8331c427b2d82c4b48eb31992a710c426d1c2b8
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-09-21 18:58:58 +0300

    Add files via upload

---
+++ Streams, Files and Directories - Lab/01.OddLines/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace OddLines
+{
+    public class OddLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\Files\input.txt";
+            string outputFilePath = @"..\..\..\Files\output.txt";
+
+            ExtractOddLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+            string[] lines = File.ReadAllLines(inputFilePath);
+
+            using (StreamWriter writer = new StreamWriter(outputFilePath))
+            {
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    if (i % 2 != 0)
+                    {
+                        writer.WriteLine(lines[i]);
+                    }
+                }
+            }
+        }
+    }
+}
