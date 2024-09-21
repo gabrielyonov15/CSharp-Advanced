ca438dd620b4ef75e14761240598effb7f666e5f
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-09-21 19:02:22 +0300

    Add files via upload

---
+++ Streams, Files and Directories - Lab/07.FolderSize/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace FolderSize
+{
+    using System;
+    using System.IO;
+    public class FolderSize
+    {
+        static void Main(string[] args)
+        {
+            string folderPath = @"..\..\..\Files\TestFolder";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            GetFolderSize(folderPath, outputPath);
+        }
+        public static void GetFolderSize(string folderPath, string outputFilePath)
+        {
+            long totalSize = 0;
+            if (Directory.Exists(folderPath))
+            {
+                DirectoryInfo directoryInfo = new DirectoryInfo(folderPath);
+                FileInfo[] files = directoryInfo.GetFiles("*", SearchOption.AllDirectories);
+                foreach (FileInfo file in files)
+                {
+                    totalSize += file.Length; 
+                }
+                double sizeInKB = totalSize / 1024.0;
+                File.WriteAllText(outputFilePath, $"{sizeInKB} KB");
+            }
+            else
+            {
+                File.WriteAllText(outputFilePath, "Папката не съществува.");
+            }
+        }
+    }
+}
