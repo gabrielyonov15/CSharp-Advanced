3a4871d8bfebca84d1cad3715e88eb0a0a0d7b6d
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-09-21 19:01:39 +0300

    Add files via upload

---
+++ Streams, Files and Directories - Lab/05.ExtractSpecialBytes/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ExtractSpecialBytes
+{
+    using System;
+    using System.IO;
+    public class ExtractSpecialBytes
+    {
+        static void Main()
+        {
+            string binaryFilePath = @"..\..\..\Files\example.png";
+            string bytesFilePath = @"..\..\..\Files\bytes.txt";
+            string outputPath = @"..\..\..\Files\output.bin";
+
+            ExtractBytesFromBinaryFile(binaryFilePath, bytesFilePath, outputPath);
+        }
+        public static void ExtractBytesFromBinaryFile(string binaryFilePath, string bytesFilePath, string outputPath)
+        {
+            List<byte> targetBytes = File.ReadAllLines(bytesFilePath)
+                                        .Select(byte.Parse)
+                                        .ToList();
+            byte[] binaryFileContent = File.ReadAllBytes(binaryFilePath);
+            List<byte> extractedBytes = new List<byte>();
+            foreach (byte b in binaryFileContent)
+            {
+                if (targetBytes.Contains(b))
+                {
+                    extractedBytes.Add(b);
+                }
+            }
+            File.WriteAllBytes(outputPath, extractedBytes.ToArray());
+        }
+    }
+}
---
+++ Streams, Files and Directories - Lab/06.Split,MergeBinaryFiles/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace SplitMergeBinaryFile
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+
+    public class SplitMergeBinaryFile
+    {
+        static void Main()
+        {
+            string sourceFilePath = @"..\..\..\Files\example.png";
+            string joinedFilePath = @"..\..\..\Files\example-joined.png";
+            string partOnePath = @"..\..\..\Files\part-1.bin";
+            string partTwoPath = @"..\..\..\Files\part-2.bin";
+
+            SplitBinaryFile(sourceFilePath, partOnePath, partTwoPath);
+            MergeBinaryFiles(partOnePath, partTwoPath, joinedFilePath);
+        }
+        public static void SplitBinaryFile(string sourceFilePath, string partOneFilePath, string partTwoFilePath)
+        {
+            byte[] fileBytes = File.ReadAllBytes(sourceFilePath);
+            int totalBytes = fileBytes.Length;
+            int partOneSize = (totalBytes + 1) / 2; 
+            int partTwoSize = totalBytes / 2;
+            using (FileStream partOneStream = new FileStream(partOneFilePath, FileMode.Create))
+            {
+                partOneStream.Write(fileBytes, 0, partOneSize);
+            }
+            using (FileStream partTwoStream = new FileStream(partTwoFilePath, FileMode.Create))
+            {
+                partTwoStream.Write(fileBytes, partOneSize, partTwoSize);
+            }
+        }
+        public static void MergeBinaryFiles(string partOneFilePath, string partTwoFilePath, string joinedFilePath)
+        {
+            using (FileStream joinedStream = new FileStream(joinedFilePath, FileMode.Create))
+            {
+                using (FileStream partOneStream = new FileStream(partOneFilePath, FileMode.Open))
+                {
+                    partOneStream.CopyTo(joinedStream);
+                }
+                using (FileStream partTwoStream = new FileStream(partTwoFilePath, FileMode.Open))
+                {
+                    partTwoStream.CopyTo(joinedStream);
+                }
+            }
+        }
+    }
+}
