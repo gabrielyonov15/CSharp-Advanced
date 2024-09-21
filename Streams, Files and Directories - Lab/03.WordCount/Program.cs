236ed9c9c91e7eb941e99c227dfa8561365dab66
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-09-21 19:00:36 +0300

    Add files via upload

---
+++ Streams, Files and Directories - Lab/03.WordCount/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace WordCount
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    public class WordCount
+    {
+        static void Main()
+        {
+            string wordPath = @"..\..\..\Files\words.txt";
+            string textPath = @"..\..\..\Files\text.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            CalculateWordCounts(wordPath, textPath, outputPath);
+        }
+        public static void CalculateWordCounts(string wordsFilePath, string textFilePath, string outputFilePath)
+        {
+            string[] words = File.ReadAllLines(wordsFilePath);
+            string text = File.ReadAllText(textFilePath).ToLower();
+            Dictionary<string, int> wordCounts = new Dictionary<string, int>();
+            foreach (string word in words)
+            {
+                wordCounts[word.ToLower()] = 0;
+            }
+            string[] textWords = text
+                .Split(new char[] { ' ', '-', ',', '.', '!', '?', ';', ':' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string word in textWords)
+            {
+                if (wordCounts.ContainsKey(word))
+                {
+                    wordCounts[word]++;
+                }
+            }
+            var sortedWordCounts = wordCounts.OrderByDescending(w => w.Value);
+            using (StreamWriter writer = new StreamWriter(outputFilePath))
+            {
+                foreach (var kvp in sortedWordCounts)
+                {
+                    writer.WriteLine($"{kvp.Key} - {kvp.Value}");
+                }
+            }
+        }
+    }
+}
---
+++ Streams, Files and Directories - Lab/04.MergeTextFiles/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace MergeFiles
+{
+    using System;
+    using System.IO;
+    public class MergeFiles
+    {
+        static void Main()
+        {
+            var firstInputFilePath = @"..\..\..\Files\input1.txt";
+            var secondInputFilePath = @"..\..\..\Files\input2.txt";
+            var outputFilePath = @"..\..\..\Files\output.txt";
+
+            MergeTextFiles(firstInputFilePath, secondInputFilePath, outputFilePath);
+        }
+        public static void MergeTextFiles(string firstInputFilePath, string secondInputFilePath, string outputFilePath)
+        {
+            string[] firstFileLines = File.ReadAllLines(firstInputFilePath);
+            string[] secondFileLines = File.ReadAllLines(secondInputFilePath);
+            using (StreamWriter writer = new StreamWriter(outputFilePath))
+            {
+                int maxLines = Math.Max(firstFileLines.Length, secondFileLines.Length);
+                for (int i = 0; i < maxLines; i++)
+                {
+                    if (i < firstFileLines.Length)
+                    {
+                        writer.WriteLine(firstFileLines[i]);
+                    }
+                    if (i < secondFileLines.Length)
+                    {
+                        writer.WriteLine(secondFileLines[i]);
+                    }
+                }
+            }
+        }
+    }
+}
