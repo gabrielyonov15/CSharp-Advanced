837417a06a5572acb45a4c5be5ede5a5b80ce6ee
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-09-21 19:04:03 +0300

    Add files via upload

---
+++ Streams, Files and Directories - Exercises/01.EvenLines/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace EvenLines
+{
+    using System;
+    public class EvenLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+
+            Console.WriteLine(ProcessLines(inputFilePath));
+        }
+        public static string ProcessLines(string inputFilePath)
+        {
+            string result = "";
+            int lineNumber = 0;
+            using (StreamReader reader = new StreamReader(inputFilePath))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (lineNumber % 2 == 0)
+                    {
+                        line = line.Replace("-", "@")
+                                   .Replace(", ", "@")
+                                   .Replace(". ", "@")
+                                   .Replace("! ", "@")
+                                   .Replace("? ", "@");
+                        string[] words = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                        Array.Reverse(words);
+                        result += string.Join(" ", words) + Environment.NewLine;
+                    }
+                    lineNumber++;
+                }
+            }
+            return result.Trim(); 
+        }
+    }
+}
---
+++ Streams, Files and Directories - Exercises/02.LineNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace LineNumbers
+{
+    using System;
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+            string outputFilePath = @"..\..\..\output.txt";
+
+            ProcessLines(inputFilePath, outputFilePath);
+        }
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            string[] lines = File.ReadAllLines(inputFilePath);
+            string[] outputLines = new string[lines.Length];
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string line = lines[i];
+                int letterCount = line.Count(char.IsLetterOrDigit); 
+                int punctuationCount = line.Count(char.IsPunctuation); 
+                outputLines[i] = $"Line {i + 1}: {line} ({letterCount})({punctuationCount})";
+            }
+            File.WriteAllLines(outputFilePath, outputLines);
+        }
+    }
+} 
