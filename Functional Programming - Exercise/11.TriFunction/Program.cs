5497c5aa7a43a617777e11216a32b9b4d1b95c98
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-09-27 20:06:57 +0300

    Add files via upload

---
+++ Functional Programming - Exercise/11.TriFunction/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _11.TriFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> names = Console.ReadLine().Split().ToList();
+            string result = FindName(names, n, IsSumGreaterThanOrEqual);
+            Console.WriteLine(result);
+        }
+        static string FindName(List<string> names, int n, Func<string, int, bool> criteria)
+        {
+            foreach (var name in names)
+            {
+                if (criteria(name, n))
+                {
+                    return name; 
+                }
+            }
+            return null;
+        }
+        static bool IsSumGreaterThanOrEqual(string name, int n)
+        {
+            int sum = name.Sum(c => (int)c); 
+            return sum >= n; 
+        }
+    }
+}
