b2b11a69f3d003a92ace77add26d400c816fd1ea
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-09-28 11:36:27 +0300

    Add files via upload

---
+++ Defining Classes - Lab/01.Car/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Car car = new Car();
+            car.Make = "Toyota";
+            car.Model = "Corolla";
+            car.Year = 2020;
+            Console.WriteLine($"Make: {car.Make}");
+            Console.WriteLine($"Model: {car.Model}");
+            Console.WriteLine($"Year: {car.Year}");
+        }
+    }
+}
---
+++ Defining Classes - Lab/03.CarConstructors/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Car car1 = new Car();
+            Console.WriteLine(car1.WhoAmI());
+            Car car2 = new Car("BMW", "X5", 2022);
+            Console.WriteLine(car2.WhoAmI());
+            Car car3 = new Car("Audi", "A4", 2023, 60, 7.5);
+            Console.WriteLine(car3.WhoAmI());
+        }
+    }
+}
