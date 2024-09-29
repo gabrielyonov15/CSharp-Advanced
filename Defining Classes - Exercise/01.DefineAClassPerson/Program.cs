b2b99acd2d23b849f90bfa54c551b9021071c44a
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-09-29 19:31:21 +0300

    Add files via upload

---
+++ Defining Classes - Exercise/01.DefineAClassPerson/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Person person1 = new Person("Peter", 20);
+            Person person2 = new Person("George", 18);
+            Person person3 = new Person("Jose", 43);
+            Console.WriteLine($"Name: {person1.Name}, Age: {person1.Age}");
+            Console.WriteLine($"Name: {person2.Name}, Age: {person2.Age}");
+            Console.WriteLine($"Name: {person3.Name}, Age: {person3.Age}");
+        }
+    }
+}
