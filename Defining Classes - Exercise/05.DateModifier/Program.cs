69ba2ed8ebaf863644b3b52d18c800b2f887c0d1
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-09-29 19:32:41 +0300

    Add files via upload

---
+++ Defining Classes - Exercise/05.DateModifier/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _05.DateModifier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstDate = Console.ReadLine();
+            string secondDate = Console.ReadLine();
+            Console.WriteLine(DateModifier.GetDaysBetweenDates(firstDate, secondDate));
+        }
+    }
+}
