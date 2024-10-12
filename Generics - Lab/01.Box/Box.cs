b694d914edac11babc3bd6b342f5da9bf206d6e5
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-10-12 15:54:22 +0300

    Add files via upload

---
+++ Generics - Lab/01.Box/Box.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private List<T> elements;
+        public Box()
+        {
+            elements = new List<T>();
+        }
+        public void Add(T element)
+        {
+            elements.Add(element);
+        }
+
+        public T Remove()
+        {
+            if (elements.Count == 0)
+            {
+                throw new InvalidOperationException("Cannot remove from an empty box.");
+            }
+            T element = elements[elements.Count - 1];
+            elements.RemoveAt(elements.Count - 1);
+            return element;
+        }
+        public int Count
+        {
+            get { return elements.Count; }
+        }
+    }
+}
