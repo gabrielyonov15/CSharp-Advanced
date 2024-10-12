ec63507ac898aec8b5cc221028004188ca34c5bd
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-10-12 15:55:42 +0300

    Add files via upload

---
+++ Generics - Lab/03.GenericScale/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GenericScale
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            EqualityScale<int> intScale = new EqualityScale<int>(5, 5);
+            Console.WriteLine(intScale.AreEqual());  
+            EqualityScale<string> stringScale = new EqualityScale<string>("Pesho", "Pesho");
+            Console.WriteLine(stringScale.AreEqual());  
+            EqualityScale<int> intScaleDifferent = new EqualityScale<int>(5, 10);
+            Console.WriteLine(intScaleDifferent.AreEqual());  
+        }
+    }
+}
