e816af4f12f002e1bf38f16b60403f459d065318
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-10-14 16:05:40 +0300

    Add files via upload

---
+++ Iterators and Comparators - Lab/04.BookComparer/BookComparator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorsAndComparators
+{
+    public class BookComparator : IComparer<Book>
+    {
+        public int Compare(Book x, Book y)
+        {
+            int titleComparison = x.Title.CompareTo(y.Title);
+            if (titleComparison != 0)
+            {
+                return titleComparison;
+            }
+            return y.Year.CompareTo(x.Year);
+        }
+    }
+}
