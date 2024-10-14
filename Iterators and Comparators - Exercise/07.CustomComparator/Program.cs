b824dee8b7c55db1e9a323b15a122099af683e4f
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-10-14 18:16:56 +0300

    Add files via upload

---
+++ Iterators and Comparators - Exercise/07.CustomComparator/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _07.CustomComparator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            Array.Sort(numbers, Compare);
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+        public static int Compare(int x, int y)
+        {
+            if (x % 2 == 0 && y % 2 != 0)
+            {
+                return -1;
+            }
+            else
+            if (x % 2 != 0 && y % 2 == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return x.CompareTo(y);
+            }
+        }
+    }
+}
