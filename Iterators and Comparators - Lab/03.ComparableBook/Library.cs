079b5c64f58fd8c360d73e410cca9a0d04f27e4a
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-10-14 16:04:44 +0300

    Add files via upload

---
+++ Iterators and Comparators - Lab/03.ComparableBook/Library.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books;
+        public Library(params Book[] books)
+        {
+            this.books = new List<Book>(books);
+            this.books.Sort(); 
+        }
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryIterator(this.books);
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+        private class LibraryIterator : IEnumerator<Book>
+        {
+            private List<Book> books;
+            private int currentIndex;
+            public LibraryIterator(List<Book> books)
+            {
+                this.books = books;
+                this.currentIndex = -1;
+            }
+            public Book Current
+            {
+                get
+                {
+                    if (currentIndex < 0 || currentIndex >= books.Count)
+                    {
+                        throw new InvalidOperationException();
+                    }
+                    return books[currentIndex];
+                }
+            }
+            object IEnumerator.Current => this.Current;
+            public bool MoveNext()
+            {
+                return ++currentIndex < books.Count;
+            }
+            public void Reset()
+            {
+                currentIndex = -1;
+            }
+            public void Dispose() { }
+        }
+    }
+}
